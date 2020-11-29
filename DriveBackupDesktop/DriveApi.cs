e353c9a48b322b9e4bebef0ce46f6c5f2424bd49
fferreira <fferreira@symplicity.com>
2020-11-29 00:11:21 -0300

    Choose folder dialog ok

--- DriveBackupDesktop/DriveApi.cs
+++ DriveBackupDesktop/DriveApi.cs
@@ -16,5 +16,87 @@ namespace DriveBackupDesktop
 {
     class DriveApi
     {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/drive-dotnet-quickstart.json
+        //static string[] Scopes = { DriveService.Scope.DriveReadonly };
+        static string[] Scopes = { DriveService.Scope.Drive };
+        static string ApplicationName = "Drive Backup Desktop";
+
+        public void StartBackup()
+        {
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Drive API googleDriveService.
+            var googleDriveService = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            // Define parameters of request.
+            /*FilesResource.ListRequest listRequest = googleDriveService.Files.List();
+            listRequest.PageSize = 10;
+            listRequest.Fields = "nextPageToken, files(id, name)";*/
+
+            // List files.
+            //IList<Google.Apis.Drive.v3.Data.File> files = listRequest.Execute().Files;
+            
+            string dateToday = (string)DateTime.UtcNow.ToString("u");
+            Dictionary<string, string> newFolder = CreateFolder("NewFolder_" + dateToday, googleDriveService);
+            UploadBasic("D:\\temp\\001-small-dummy.zip", newFolder["id"], googleDriveService);
+
+        }
+
+        private static Dictionary<string, string> CreateFolder(string folderName, DriveService googleDriveService)
+        {
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File()
+            {
+                Name = folderName,
+                MimeType = "application/vnd.google-apps.folder"
+            };
+            var request = googleDriveService.Files.Create(fileMetadata);
+            request.Fields = "id, name";
+            var file = request.Execute();
+            //Console.WriteLine("Folder ID: " + file.Id);
+            Dictionary<string, string> createdFolder = new Dictionary<string, string>();
+            createdFolder.Add("name", file.Name);
+            createdFolder.Add("id", file.Id);
+            return createdFolder;
+        }
+
+        private static void UploadBasic(string path, string remoteFolderId, DriveService googleDriveService)
+        {
+            var fileMetadata = new Google.Apis.Drive.v3.Data.File();
+            fileMetadata.Parents = new[] { remoteFolderId };
+            fileMetadata.Name = Path.GetFileName(path);
+            fileMetadata.MimeType = "application/zip";
+            //System.Diagnostics.Debug.WriteLine(fileMetadata);
+            FilesResource.CreateMediaUpload request;
+            using (var stream = new System.IO.FileStream(path, System.IO.FileMode.Open))
+            {
+                request = googleDriveService.Files.Create(fileMetadata, stream, "application/zip");
+                request.Fields = "id";
+                request.Upload();
+            }
+
+            var file = request.ResponseBody;
+
+        }
+
     }
 }
--- DriveBackupDesktop/MainWindow.xaml.cs
+++ DriveBackupDesktop/MainWindow.xaml.cs
@@ -12,6 +12,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+//using Microsoft.WindowsAPICodePack.Dialogs;
+//using System.Windows.Forms;
+//using Microsoft.Win32;
+
+// Expose class to use here
+using DriveBackupDesktop;
+
 
 namespace DriveBackupDesktop
 {
@@ -24,5 +31,64 @@ namespace DriveBackupDesktop
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var backup = new DriveApi();
+            backup.StartBackup();
+        }
+
+
+        private void Choose_Folder_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+            if (dialog.ShowDialog(this).GetValueOrDefault())
+            {
+                String path = dialog.SelectedPath;
+                UserSelectedPath(path);
+                //System.Diagnostics.Debug.WriteLine(path);
+            }
+
+        }
+
+        public String UserSelectedPath(string path)
+        {
+            String UserSelectedPath = path;
+            System.Diagnostics.Debug.WriteLine(path);
+            return UserSelectedPath;
+        }
+
+
+
+
+
+
+
+
+        /*
+        private void Choose_Folder_Click(object sender, RoutedEventArgs e)
+        {
+            var folderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            folderDialog.ShowDialog();
+
+        }
+*/
+        /*private void Choose_Folder_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new OpenFileDialog();
+            if (fileDialog.ShowDialog() == true)
+            {
+                String filePath = fileDialog.FileName;
+                System.Diagnostics.Debug.WriteLine(filePath);
+
+            }
+
+        }*/
+
+
+
+
+
+
     }
 }
